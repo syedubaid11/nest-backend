2c3a8da20fe42ea5d3af55ccb4f81c1713c6401a
syedubaid <syedabuubaid@gmail.com>
2025-01-05 21:42:10 +0530

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ],
   providers: [AppService],
 })
 export class AppModule {}
